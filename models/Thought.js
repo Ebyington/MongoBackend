b80f76d284221315a776836131094e6a57945f42
Eric <ericbyington33@gmail.com>
2022-11-21 18:25:19 -0500

    models

---
+++ models/Thought.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require("./Reaction");
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            max_length: 280,
+            min_length: 1,
+            required: true,
+
+
+        }, 
+        createdAt: {
+            tyoe: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions:
+            [reactionSchema] 
+    },
+    {
+      toJSON: {
+        getters: true,
+      },
+      id: false,
+    }
+  );
+const Thought = model("Thought", thoughtSchema);
+module.exports = Thought;
---
+++ routes/api/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const thoughtRoutes = require('./thoughtRoutes');
+const userRoutes = require('./userRoutes');
+
+router.use('/thought', thoughtRoutes);
+router.use('/users', userRoutes);
+module.exports = router;
